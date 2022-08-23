5dce8f7c556b87c6a24ec0d7f4a2971ac4accbb6
SamarinaAS <a.samarina1997@yandex.ru>
2022-08-23 21:20:46 +0300

    added method delete

--- Cpp/Linked_list/lincked_list.cpp
+++ Cpp/Linked_list/lincked_list.cpp
@@ -1,7 +1,7 @@
  #include <iostream>
 
 class Node{
-    public: Node *next;
+    Node *next;
     int data;
     public:
     Node(int data_){
@@ -16,13 +16,38 @@ class Node{
         current->next = new Node(data_);
     }
     int getData(){
-        return this->data;
+       return this->data;
     }
-    Node* getNext(){
-        return this->next;
+
+//удаляет все узлы с данным значением
+    static void deleteNode(Node* &head, int data_){
+        while ( head!=nullptr && head->data == data_){
+            Node *temp = head->next;
+            delete head;
+            head = temp;
+        }
+        if (head==nullptr){
+            return;
+        }
+        Node *newHead = head;
+        while(newHead->next!=nullptr){
+            if (newHead->next->data == data_){
+                Node *temp = newHead->next->next;
+                delete newHead->next;
+                newHead->next = temp;
+            }else{
+                newHead = newHead->next;
+            }
+        }
+
     }
+    
     void print(){
         std::cout<<"Односвязный список: "<<std::endl;
+        if (this == nullptr){
+            std::cout<<"Список пуст"<<std::endl;
+            return;
+        }
         Node* current = this;
         while (current->next!=nullptr){
             std::cout<< current->getData()<< " ";
@@ -32,12 +57,17 @@ class Node{
     }
 };
 
-
-
 int main(){
-    Node *node = new Node(1);
-    node->append(2);
-    node->append(3);
-    node->print();
+    Node *head = new Node(2);   
+    head->append(2);
+    head->append(1);
+    head->append(3);
+    head->append(1);
+    head->append(1);
+    head->print();
+    int i = 1;
+    Node::deleteNode(head, i);
+    std::cout<<"Удаляем узел "<<i<<std::endl;
+    head->print();
     return 0;
 };
