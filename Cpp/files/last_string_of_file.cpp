229a1bd27945f3eb88c60d0f2f1058e8c4fae7a7
SamarinaAS <a.samarina1997@yandex.ru>
2022-08-23 15:26:23 +0300

    added reading filename from command line

--- Cpp/files/last_string_of_file.cpp
+++ Cpp/files/last_string_of_file.cpp
@@ -1,5 +1,5 @@
 //напишите метод для вывода последних k строк входного файла
-
+//подавать файл в строку при вызове программы?
 #include <iostream>
 #include <fstream>
 //решение в лоб
@@ -16,9 +16,13 @@ void read_last_strings(std::string filename){
         }
     }
     in.close();
-    std::cout<<"Общее количество строк равно: "<< n << ". Введите количество последних k строк, которое вы хотите вывести:" << std::endl << "k = ";
     int k;
+    std::cout<<"Общее количество строк в файле равно: "<< n << ". Введите количество последних k строк, которое вы хотите вывести:" << std::endl << "k = ";
     std:: cin >> k;
+    while (k > n){
+        std::cout<<"Общее количество строк в файле меньше "<<k<<", оно равно "<<n<<". Введите k, меньшее или равное количеству строк в файле: "<<std::endl<<"k = ";
+        std:: cin >> k;
+    }
          // закрываем файл
     in.open(filename);
     std::cout<<"Выводим "<<k<<" последних строк файла:"<<std::endl; 
@@ -36,10 +40,14 @@ void read_last_strings(std::string filename){
     in.close();  
 }
 
-int main(){
+int main(int argc, char* argv[]){
     //печатаем k последних строк файла
+    if (argc < 2){
+       std::cout<<"Название файла не введено"<<std::endl;
+       return 0;
+    }
     int k = 3;
-    read_last_strings("name.txt");
+    read_last_strings(argv[1]);
     return 0;
 }
  
