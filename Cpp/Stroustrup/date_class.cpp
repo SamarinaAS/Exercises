e80299e674a501ebe0c0a8de4e447703aafa386a
SamarinaAS <a.samarina1997@yandex.ru>
2022-09-05 17:05:25 +0300

    added check for incorrect day

--- Cpp/Stroustrup/date_class.cpp
+++ Cpp/Stroustrup/date_class.cpp
@@ -1,56 +1,101 @@
+#include <iostream>
 class Date
 {
-    private:
-    int day, month, year;
-    enum months {jan = 1, feb, mar, aip, may, jun, jul, aug, sep, oct, nov, dec};
-    public:
-    Date(int day, int month, int year);
-    static bool isYearLeap(int year);
-    void addYear(int n);
-    void addMonth(int n);
-    // void addDay(int n);   
+private:
+    unsigned day, month, year;
+    enum months
+    {
+        jan = 1,
+        feb,
+        mar,
+        aip,
+        may,
+        jun,
+        jul,
+        aug,
+        sep,
+        oct,
+        nov,
+        dec
+    };
+    void isDateCorrect(unsigned day, unsigned month, unsigned year);
+
+public:
+    Date(unsigned day, unsigned month, unsigned year);
+    static bool isYearLeap(unsigned year);
+    void addYear(unsigned n);
+    void addMonth(unsigned n);
+    // void addDay(unsigned n);
 };
 
-Date::Date(int day, int month, int year){
-        this->day = day;
-        this->month = month;
-        this->year = year;
-    };
-bool Date::isYearLeap(int year){
-        if ((year)%400==0 || year%4==0 && year%100!=0){
-            return true;
-        }
-        return false;
+Date::Date(unsigned day, unsigned month, unsigned year)
+{
+    isDateCorrect(day, month, year);
+    this->day = day;
+    this->month = month;
+    this->year = year;
+};
+void Date::isDateCorrect(unsigned day, unsigned month, unsigned year)
+{
+    if (month == months::feb && (day == 29 && !isYearLeap(year) || day > 29) 
+    || (month == months::aip || month == months::jun || month == months::sep || month == months::nov) && day == 31
+    || !(month >= months::jan && month <= months::dec) || day == 0 || day > 31 || year == 0){
+        throw "Incorrect date";
     }
-void Date::addYear(int n){
-        if (month == months::feb && !isYearLeap(year+n)){
+}
+bool Date::isYearLeap(unsigned year)
+{
+    if ((year) % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+    {
+        return true;
+    }
+    return false;
+}
+void Date::addYear(unsigned n)
+{
+    if (month == months::feb && !isYearLeap(year + n))
+    {
+        day = 28;
+    }
+    year += n;
+};
+void Date::addMonth(unsigned n)
+{
+    year += (month + n) / 12;
+    month = (month + n) % 12;
+    if (day > 28)
+    {
+        if (month == months::feb && !isYearLeap(year))
+        {
             day = 28;
         }
-        year += n;
-    };
-void Date::addMonth(int n){
-        year+=(month+n)/12;
-        month =(month+n)%12;
-        if(day>28){
-            if(month == months::feb && !isYearLeap(year)){
-                day = 28;
-            }else if (day == 31){
-                if (month == months::aip||month == months::jun || month == months::sep || month == months::nov){
-                    day = 30;
-                }
+        else if (day == 31)
+        {
+            if (month == months::aip || month == months::jun || month == months::sep || month == months::nov)
+            {
+                day = 30;
             }
         }
-    };
-    // void Date::addDay(int n){
+    }
+};
+// void Date::addDay(unsigned n){
+//     while(){
 
-    // };
+//     }
+//     year+=(day+n)/
+// };
 
-int main (){
+int main()
+{
     Date *my = new Date(29, 2, 2024);
     my->addYear(5);
     Date *my1 = new Date(31, 3, 2024);
     my1->addMonth(1);
     my1->addMonth(10);
+    try{
+        Date *my2 = new Date(32, 3, 2024);
+    }catch (...){
+        std::cout<<"Entered incorrect date"<<std::endl;
+    }
     return 0;
 }
-
