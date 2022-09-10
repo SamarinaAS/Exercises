a1e95be469c440fdc35b325e04e6adb3f894a9be
SamarinaAS <a.samarina1997@yandex.ru>
2022-09-10 19:38:25 +0300

    added checking the array of intervals for sorting

--- Cpp/Stroustrup/histogram.cpp
+++ Cpp/Stroustrup/histogram.cpp
@@ -1,3 +1,6 @@
+//в конструктор вектор интервалов нужно подавать в отсортированном порядке - иначе
+//вызовется исключение
+
 #include <vector>
 #include <stdio.h>
 #include <algorithm>
@@ -9,7 +12,6 @@ class Histogram{
     std::vector <int> data;
     std::vector <int> intervals;
     std::map<std::string, int> histogramMap;
-    //std::vector <int vector <int>> tempIntervals;
     public:
     Histogram(std::vector <int> data, std::vector <int> intervals);
     void printHistogram () const;
@@ -19,20 +21,25 @@ Histogram::Histogram(std::vector <int> data, std::vector <int> intervals){
     if (data.size()==0 || intervals.size()==0){
         throw "Empty data or intervals";
     }
-        for(int i=0; i<intervals.size()-1; i++){
-            int left = intervals[i];
-            int right = intervals[i+1];
-            if (right<left-1){
-                throw "Incorrect interval boundary";
+    std::vector <int> copyData = data;
+    sort (copyData.begin(), copyData.end());
+    if (copyData != data){
+        throw "Wrong sequence of interval boundaries";
+    }
+    for(int i=0; i<intervals.size()-1; i++){
+        int left = intervals[i];
+        int right = intervals[i+1];
+        if (right<left-1){
+            throw "Incorrect interval boundary";
+        }
+        
+        std::string strInterval = "[" + std::to_string(left) + ", " + std::to_string(right-1) + "]";
+        histogramMap[strInterval]= 0;
+        for (int i = 0; i<data.size(); i++){
+            if (data[i]>=left && data[i]<right){
+                histogramMap[strInterval]++;
             }
-            
-            std::string strInterval = "[" + std::to_string(left) + ", " + std::to_string(right-1) + "]";
-            histogramMap[strInterval]= 0;
-            for (int i = 0; i<data.size(); i++){
-                if (data[i]>=left && data[i]<right){
-                    histogramMap[strInterval]++;
-                }
-            }
+        }
     }
     this->data = data;
     this->intervals = intervals;
@@ -46,7 +53,7 @@ void Histogram::printHistogram() const{
  }
 
 int main(){
-    std::vector <int> data = {1, 2, 3, 4, 5, 6};
+    std::vector <int> data = {2, 1, 3, 4, 5, 6};
     std::vector <int> intervals = {1, 3, 6};
     Histogram* hist = new Histogram(data, intervals);
     hist->printHistogram();
