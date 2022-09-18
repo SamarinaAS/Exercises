30941114a7f4631ac7bf736fad3aa41351e75e6d
SamarinaAS <a.samarina1997@yandex.ru>
2022-09-18 23:52:06 +0300

    added fields and change construcotor in CallCenterEmployee and Operator

--- Cpp/LuckmanMcDowell/employee.cpp
+++ Cpp/LuckmanMcDowell/employee.cpp
@@ -3,28 +3,49 @@
 //звонок, он автоматически перенаправляется менеджеру, если и тот занят, то директору. Разработайте
 //классы и структуры данных для этой задачи. Реализуйте метод dispatchCall(), который перенаправляет
 //звонок первому свободному сотруднику.
-class CallCenterEmployee{
-    public:
-    bool free;
-    CallCenterEmployee();
-    CallCenterEmployee(bool free);
-    isFree();
-}
 
-CallCenterEmployee::CallCenterEmployee(){true};
-CallCenterEmployee::CallCenterEmployee(bool free){
-    this->free = free;
+#include <string>
+class CallCenterEmployee{
+    private:
+    bool free;
+    std::string firstName;
+    std::string lastName;
+    public:
+    CallCenterEmployee(std::string firstName, std::string lastName, bool free);
+    bool isFree();
 };
 
+CallCenterEmployee::CallCenterEmployee(std::string firstName, std::string lastName, bool free){
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->free = free;
+}
+
+bool CallCenterEmployee::isFree(){
+    return free;
+}
+
 class Operator : public CallCenterEmployee{
-    Operator(){true};
+    private:
+    int number;
+    Operator(std::string firstName, std::string lastName, bool free, int number);
 
+};
+
+Operator::Operator(std::string firstName, std::string lastName, bool free, int number)
+:CallCenterEmployee(firstName, lastName, free){
+    this->number = number;
 }
 
-class Manager : public CallCenterEmployee{
-    Manager(){true};
-}
+// class Manager : public CallCenterEmployee{
+//     Manager(){true};
+// }
 
-class Director : public CallCenterEmployee{
-    Director(){true};
+// class Director : public CallCenterEmployee{
+//     Director(){true};
+// }
+
+
+int main (){
+    return 0;
 }
